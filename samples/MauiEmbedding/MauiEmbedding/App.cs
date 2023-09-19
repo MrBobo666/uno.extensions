27d7c5991ce43309ceb7b073eec98c99b6b6c056
Yuan <yuan.hsieh@hp.com>
2023-09-19 13:46:08 +0800

    test

--- samples/MauiEmbedding/MauiEmbedding/App.cs
+++ samples/MauiEmbedding/MauiEmbedding/App.cs
@@ -83,6 +83,7 @@ public class App : EmbeddingApplication
 				Nested: new RouteMap[]
 				{
 					new RouteMap("Main", View: views.FindByViewModel<MainViewModel>()),
+					new RouteMap("Main", View: views.FindByViewModel<MainViewModel>()),
 				}
 			)
 		);
